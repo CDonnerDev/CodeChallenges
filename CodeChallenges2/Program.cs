b14b5710c93cbf369bcaea59b0ef953b08cc93e7
CDonnerDev <Colson.Donner1@outlook.com>
2022-10-26 15:23:52 -0500

    challenges

---
+++ CodeChallenges2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeChallenges2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            int runny = 0;
+             //Console.WriteLine($"{a}\n{b}");
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    c = a + b;
+            //    a = c + b;
+            //    b = a + c;
+            //    //Console.WriteLine($"{c}\n{a}\n{b}");
+            //    //if (a % 2 == 0 && b % 2 ==0 && c % 2 == 0)
+            //    //{
+            //    //c = a + b;
+            //    //a = c + b;
+            //    //b = a + c;
+
+            //    //}
+            //    Console.WriteLine($"{c}\n{a}\n{b}");
+                
+            //}
+            do
+            {
+
+
+                a = b;
+                b = c;
+                c = a + b;
+              
+                if (c % 2 == 0)
+                {
+                Console.WriteLine(c);
+                    runny += c;
+                }
+            } while (c <= 4000000);
+            Console.WriteLine("The final sum = " + runny);
+            
+        }
+    }
+}
--- Multiples3Sand5S/Multiple3sand5s.cs
+++ Multiples3Sand5S/Multiple3sand5s.cs
@@ -1,19 +1,21 @@
 ﻿namespace Multiples3Sand5S
 {
-    internal class Program
+    internal class Multiple3sand5s
     {
         static void Main(string[] args)
         {
             List<int> numbers = new List<int>();
             int numberToBeAdded = 0;
             Console.WriteLine(numberToBeAdded % 3 == 0);
-            for (int i = 0; i <= 1000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 numberToBeAdded++;
+
                 if (numberToBeAdded % 3 == 0 || numberToBeAdded % 5 == 0)
                 {
                     numbers.Add(numberToBeAdded);
-                    Console.WriteLine(numberToBeAdded);
+                    Console.WriteLine(numberToBeAdded + "|" + i);
+
                 }
                 
             }
