dee1e764830229f2741143861845f4f3ff3203b1
CDonnerDev <Colson.Donner1@outlook.com>
2022-10-26 13:09:28 -0500

    Completed Challenge 1

--- Multiples3Sand5S/Program.cs
+++ Multiples3Sand5S/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int>();
+            int numberToBeAdded = 0;
+            Console.WriteLine(numberToBeAdded % 3 == 0);
+            for (int i = 0; i <= 1000; i++)
+            {
+                numberToBeAdded++;
+                if (numberToBeAdded % 3 == 0 || numberToBeAdded % 5 == 0)
+                {
+                    numbers.Add(numberToBeAdded);
+                    Console.WriteLine(numberToBeAdded);
+                }
+                
+            }
+            Console.WriteLine(numbers.Sum());
         }
     }
 }
