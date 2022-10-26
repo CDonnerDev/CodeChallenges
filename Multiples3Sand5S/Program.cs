9bc5016d868efc956ee0697802a2a05b6b66c281
CDonnerDev <Colson.Donner1@outlook.com>
2022-10-26 12:55:32 -0500

    Add project files.

---
+++ Multiples3Sand5S/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Multiples3Sand5S
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
